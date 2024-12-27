6541bd513184b0c12461f774b70700ae80cf1d98
oripovfakhriddin <fakhriddinoripov0731@gmail.com>
2024-12-27 23:20:54 +0500

    fixed-d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Homepage = () => {
   return (
     <div className=''>
-      <Link href='/admin'>Admin</Link>
+      <Link href='/admin'>Admin - ga o'tish</Link>
     </div>
   );
 };
