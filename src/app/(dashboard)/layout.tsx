2a804285670389193de788c2795e58d75143a62b
oripovfakhriddin <fakhriddinoripov0731@gmail.com>
2024-12-28 00:59:21 +0500

    removed menu

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,3 @@
-import Menu from "components/menu";
 import Navbar from "components/navbar";
 import Image from "next/image";
 import Link from "next/link";
@@ -18,7 +17,6 @@ export default function DashboardLayout({
           <Image src='/logo.png' alt='Stem Logo' width={32} height={32} />
           <span className='hidden lg:block font-bold'>Stem LC</span>
         </Link>
-        <Menu />
       </div>
       {/* Right */}
       <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col'>
